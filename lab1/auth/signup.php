6f7d1a5f5970db055266369bb510d9d35ec51370
gustav.ahlberg@gmail.com <gustav.ahlberg@gmail.com@cb8e7195-d7fe-5f39-bb79-97b1085b4703>
2013-01-24 15:47:52 +0000

    added proof of concept of auth method

---
+++ lab1/auth/signup.php
@@ -0,0 +1,54 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+<script type="text/javascript" src="jquery.qrcode.js"></script>
+<script type="text/javascript" src="qrcode.js"></script>
+
+<?php
+
+require_once('./tools.php');
+
+if (isset($_POST["signup"])) {
+
+	$username = $_POST["username"];
+	if (!userExists($username)) {
+		$secret = signup($username);
+
+		if ($secret > 0) {
+			echo '<input id="secret" type="hidden" name="secret" value="' . $secret . '">';
+			echo '<div id="qr"></div>';
+			echo "<div>Scan this with the special app</div>";
+
+?>
+
+<script type="text/javascript">
+secret = $("#secret").val();
+jQuery('#qr').qrcode({
+	text : secret
+});
+</script>
+
+<a href="index.php">Login</a>
+
+<?php
+		}
+	} else {
+		echo "username already exists<br>";
+		echo '<a href="signup.php">Signup</a>';
+	}
+	
+
+
+} else {
+
+?>
+
+
+<form action="" method="post">
+
+Username: <input type="text" name="username">
+<br />
+<input type="submit" name="signup" value="sign up">
+
+</form>
+<?php
+}
+?>
